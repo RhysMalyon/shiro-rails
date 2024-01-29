33fd8efc14fb682b93f4bc54ac03d8bab7d69fb8
Rhys Malyon <malyon@housingjapan.com>
2024-01-29 14:44:20 +0900

    Feat!: Add appointments destroy dependency to Customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :appointments, foreign_key: :customer_id
+  has_many :appointments, foreign_key: :customer_id, dependent: :destroy
 
   validates :first_name, :last_name, :first_name_phonetic, :last_name_phonetic, :tel, presence: true
   validates :email, presence: true, uniqueness: true
